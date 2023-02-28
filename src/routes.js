79cb7a1a42abb1b21b1be9c37d990705918ef311
Carlos Estevão <carlosestevaobs@gmail.com>
2023-02-28 10:04:59 -0300

    Alteração de dados

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,14 @@ const router = express.Router();
 // para a rota enxergar o carroController
 const CarroController = require('./controllers/CarroController');
 
-//os dados serão recebidos
+// os dados serão recebidos
 router.get('/carros', CarroController.buscarTodos);
 router.get('/carro/:codigo', CarroController.buscarUm);
+
 // os dados serão inseridos
 router.post('/carro', CarroController.inserir);
+
+// alteração de dados
+router.put('/carro/:codigo', CarroController.alterar);
 
 module.exports = router;
